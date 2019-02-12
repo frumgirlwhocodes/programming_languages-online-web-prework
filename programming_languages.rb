38539a744c6cd53d479ec58033b4d59e25d106eb
lieby weintraub <liebahinda@gmail.com>
2019-02-12 22:58:25 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
   # your code here
-end
+  new_hash={}
+ anguages.each { |style, languageInfo| 
+    languageInfo.each { |key, value|
+      if !new_hash.has_key?(key)
+        new_hash[key] = {type: value[:type], style: [style]}
+      else
+        new_hash[key][:style] << style
+      end
+    }
+  }
+  new_hash
+end	
